84272954e82971c5fca81285711483a9f77955ca
Zimpto <76706980+Zimpto@users.noreply.github.com>
2023-12-18 10:52:52 +0100

    Add files via upload
    2 Zeiler Python Code Template für schönen QR Code

---
+++ QRCode_Winterquiz.py
@@ -0,0 +1,14 @@
+# QR Codes with segno
+import segno
+
+# Create open url
+qrOpenURL = segno.make_qr("http://192.168.1.123/")
+# Save the QR Code as pdf, png...
+# dark means the color of the black squares,
+# light menas the color of the white squares
+qrOpenURL.save("Winterquiz.pdf", border = 0, 
+               dark = "black", light = "white")
+
+
+
+
