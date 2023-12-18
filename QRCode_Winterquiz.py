424f376310613bc8aa29f6ff8163301a2ee16d46
Zimpto <76706980+Zimpto@users.noreply.github.com>
2023-12-18 10:53:41 +0100

    Update QRCode_Winterquiz.py

--- QRCode_Winterquiz.py
+++ QRCode_Winterquiz.py
@@ -5,7 +5,7 @@ import segno
 qrOpenURL = segno.make_qr("http://192.168.1.123/")
 # Save the QR Code as pdf, png...
 # dark means the color of the black squares,
-# light menas the color of the white squares
+# light means the color of the white squares
 qrOpenURL.save("Winterquiz.pdf", border = 0, 
                dark = "black", light = "white")
 
