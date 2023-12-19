9b94f14aeda42d849da8b02dec507cd06c313b39
Zimpto <76706980+Zimpto@users.noreply.github.com>
2023-12-19 17:28:05 +0100

    Add files via upload
    Order für schönen QR-Code

---
+++ QR/QRCode_Winterquiz.py
@@ -0,0 +1,22 @@
+# QR Codes with segno
+
+# Source of artistic segno:
+# https://segno.readthedocs.io/en/latest/artistic-qrcodes.html
+# Source of the Snowflake
+# http://www.publicdomainfiles.com/show_file.php?id=13529242816467
+
+import segno
+
+# Create open url
+qrOpenURL = segno.make_qr("http://192.168.1.123/")
+
+# Save in an "artistic" way
+qrOpenURL.to_artistic(background="Snowflake.png", 
+                   target="SnowflakeQR.png", scale=20, border = 0)
+
+# =============================================================================
+# # Save in a normal way
+# qrOpenURL.save("Winterquiz.pdf", border = 0, 
+#                 dark = "black", light = "white")
+# =============================================================================
+
